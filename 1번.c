aa906c25a7957fc64ee81a6d24d46de65ad62aec
kim-guenyoung <101184338+kim-guenyoung@users.noreply.github.com>
2022-09-23 01:21:08 +0900

    Add files via upload

--- 1번.c
+++ 1번.c
@@ -1,13 +1,23 @@
 /*
-//1. 다음 코드의 실행 결과를 예측해 보고, 코드를 직접 실행해본 결과와 일치하는지 확인하세요.
+//1. 키를 cm단위로  입력 받아 m단위로 환산해서 출력해주는 프로그램을 작성하세요.
+
 #include <stdio.h>
+
+float cm_to_m(float height_m); //main 함수보다 먼저 출력되게 하려고 함수 호출
+
 int main(void)
 {
-	int arr[3];
-	int* pa = arr;
+	float height;
+	float result;
+	printf("키를 입력하세요(cm) : ");
+	scanf_s("%f", &height);
 
-	printf("%d %d", sizeof(arr), sizeof(pa));
-	//12, 8
-	//sizeof(배열) -> 3 * 4(타입) 따라서 12 출력
-	//sizeof(포인터) -> 아스트리크 문자 붙여서 포인터라고 지시하면 비트 수에 비례(현재 64비트 사용 중이라 8 출력)
+	result = cm_to_m(height);
+
+	printf("%0.2f", result); //소수점 둘째 자리까지만 출력
+	return 0;
+}
+float cm_to_m(float height_m)
+{
+	return height_m * 0.01;
 }*/
--- 3번.c
+++ 3번.c
@@ -1,13 +1,31 @@
-/*#include <stdio.h>
+/*
+//3. 두 개의 실수를 입력 받아 합계와 평균을 구하는 함수 1개를 만들어서 호출하세요.
+
+#include <stdio.h>
+
+float calculator(float A, float B);
+
 int main(void)
 {
-	double arrd[4] = { 12.4, 23.1, 15.7, 31.2 };
-	double* pb = arrd;
+	float a, b;
+	char oper;
+	printf("두 개의 실수 입력 : ");
+	scanf("%f %f", &a, &b);
 
-	int i;
-	for (i = 0; i < 4; i++)
-		printf("%.1lf\n", *(pb + i));
-	//arr[i] == *(arr+i) 이용!
+	printf("연산자 입력 ( + / # ) : ");
+	scanf("%s", &oper);
 
-
+	if (oper == '+')
+		printf("%f", calculator(a, b));
+	else if (oper == '#')
+		printf("%f", (calculator(a, b)) / 2);
+	else
+		return 0;
+	return 0;
+}
+float calculator(float A, float B)
+{
+	float result;
+	result = A + B;
+	return result;
 }*/
